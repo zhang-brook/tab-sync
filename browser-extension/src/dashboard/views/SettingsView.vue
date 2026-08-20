4950e2f3c6ad021b3288a73794b8d7b919f1c590
Brook Zhang <codingzhang@126.com>
2026-08-21 04:07:35 +0800

    fix: element-plus <el-link :underline="boolean"> deprecate warning

--- browser-extension/src/dashboard/views/SettingsView.vue
+++ browser-extension/src/dashboard/views/SettingsView.vue
@@ -109,7 +109,7 @@
           <el-button size="small" @click="openShortcutsPage">打开 Chrome 快捷键设置页</el-button>
           <div class="form-tip">未显示按键的功能需在 Chrome 快捷键设置页手动绑定</div>
           <div class="form-tip" v-if="true/* Chrome 浏览器 */">
-            Chrome 修改快捷键后，需<el-link type="primary" size="small" style="font-size: inherit; vertical-align: baseline;" :underline="false" @click="reloadExtension">
+            Chrome 修改快捷键后，需<el-link type="primary" size="small" style="font-size: inherit; vertical-align: baseline;" underline="never" @click="reloadExtension">
               重新加载扩展
             </el-link>后生效
           </div>
